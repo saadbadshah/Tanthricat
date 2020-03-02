1e4005637648f2d39014908030f457eacab1972e
JWelan <jameswelan467@gmail.com>
2020-03-02 21:48:54 +0000

    Update house-map.php

--- Front-End/house-map.php
+++ Front-End/house-map.php
@@ -386,7 +386,6 @@
     <!-- Additonal JS files -->
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
     <script src='https://code.jquery.com/ui/1.11.4/jquery-ui.min.js'></script>
-    <script src="js/house-map.js"></script>
     <script>
         $(function () {
 

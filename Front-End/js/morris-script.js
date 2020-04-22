e96a00d93b6e094b24f049d45b14253e16b3ce27
Perry1903 <perry_anderson@ntlworld.com>
2020-04-22 01:29:35 +0100

    Fixed array problem, tidied up, added comments

--- Front-End/js/morris-script.js
+++ Front-End/js/morris-script.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
   lineChart();
   //areaChart();
   //donutChart();
+
+  /*
+  Creates an array that will hold device objects
+  with their nicknames and calculated energy usage.
+  */
+  var devices = new Array();
   retrieveValues();
 
   $(window).resize(function() {
@@ -14,50 +20,85 @@ $(document).ready(function() {
 });
 
 
-
-var devices = new Array();
-
 function retrieveValues() {
-  // call ajax
-  var ajax = new XMLHttpRequest();
+    // Create an AJAX get request to fetch the array (in string form)
+    var ajax = new XMLHttpRequest();
+    var method = "GET";
+    var url = "Front-End/calculate-values.php";
+    var asynchronous = true;
 
-  // sending ajax request to php file containing array
-  ajax.open("GET", "../calculate-values.php", true);
-  ajax.send();
+    // Form the request and send it
+    ajax.open(method, url, asynchronous);
+    ajax.send();
 
-  // receiving response from calculate-values.php
-  ajax.onreadystatechange = function ()
-  {
-      if (this.readyState == 4 && this.status == 200)
-      {
-          // converting JSON back to array
-          devices = JSON.parse(this.responseText);
-          // console.log(values);
-      }
-
-  }
-
-  calculateEnergy();
+    // Receive the response and then convert the string back to an array, before passing it on
+    ajax.onreadystatechange = function()
+    {
+        if (this.readyState == 4 && this.status == 200)
+        {
+            var data = JSON.parse(this.responseText);
+            calculateEnergy(data)
+        }
+    }
 }
-function calculateEnergy() {
-  // Calculating energy usage for each device
-  for (var i=0; i<devices.length; i++) {
 
-    var date = new Date();
-    var time = Math.abs(date - devices[i][2]);
-    var totHours = time/3600000;
-    var energy = Math.round(totHours * devices[i][1]);
+// For each device in the array, calculate its energy usage by taking the difference
+// between the current date and the last time the device was turned on (i.e. how long
+// the device has currently been on for), convert it into hours, and then multiplying
+// by its energy rating (watts per hour) to achieve its energy usage.
+// Assign the device's nickname and its energy value as a new entry to the array of
+// devices.
+function calculateEnergy(data) {
+    var today = new Date();
+    for (var i=0; i<data.length; i++) {
+        var name = data[i].Nickname;
+
+        var d = new Date(JSON.stringify(data[i].LastOnDate));
+        var time = Math.abs(today - d);
+        var totHours = time/(3600*1000);
+
+        var rating = Number(data[i].EnergyRating);
+        var energy = Math.round(totHours * rating);
+        
+        devices[i] = {nickname:name, value:energy};
+    }
+    console.log(devices);
+}
+
+/* SEE THE FOLLOWING SUGGESTION WHEN MAPPING VALUES TO CHART
+
+var added = new Array();
+// When device value is mapped to the chart, add it to the array 'added' - this an array of all devices
+// which have been recorded on the chart in the past.
+
+// Loop through all devices and their values to be updated to the chart, for every device - if the device's nickname
+// exists in the array (i.e. indexOf returns its index in the array) then its current value will simply be added to
+// its previous value recorded on the chart.
+// However, if the device's name is not found in the array (i.e. it is a new entry to the chart) then create a new
+// entry of the device and its value on the chart.
+
+for (var i=0; i<devices.length; i++) {
+    var name = devices[i].Nickname;
+    if (added.indexOf(name) != -1) {
+        // If the device exists in the chart, just append its new
+        // energy value to its previous chart value.
+    }
+    else {
+        // Add the new device and its energy value to the chart, and then add the device to the array.
+        added.push(devices[i]);
+    }
+
+    IMPORTANT
+    Once all device values have been updated on the chart, update the TanthricatDevices table in the database to set the
+    'LastOnDate' value of the devices used to today's date by passing a list of the nicknames and updating the table accordingly.
+
+    This is to prevent the user from accessing the reports page, thus updating the chart for currently active devices, and then
+    refreshing which would cause the values to be updated again. By setting the 'LastOnDate' to the current day, this means the 'time'
+    variable used above in calculateEnergy() method will be: the current date - device LastOnDate (current day) = 0 day difference, 
+    thus meaning its energy value will be 0 and so it wont add anything to its chart value until the next day at the earliest.
     
-    devices[i] = {nickname:devices[i][0], value:energy};
- 	//console.log(devices[i]);
-  }
-
 }
-//console.log(devices[0]);
- //console.log(devices[0].value);
-
-// Use devices[index].nickname for device name
-// and devices[index].value for how much energy it has used
+*/
 
 
 function barChart() {

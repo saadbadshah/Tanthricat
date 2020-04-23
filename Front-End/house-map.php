0e7360dee346f0c6f8fbca4a05b1e238c81e8144
JWelan <jameswelan467@gmail.com>
2020-04-23 16:14:34 +0100

    nav changes

--- Front-End/house-map.php
+++ Front-End/house-map.php
@@ -69,9 +69,6 @@
                         </a>
                         <ul class="dropdown-menu extended logout dropdown-menu-right">
                             <div class="log-arrow-up"></div>
-                            <li><a href="#"><i class=" fa fa-suitcase"></i>Profile</a></li>
-                            <li><a href="#"><i class="fa fa-cog"></i> Settings</a></li>
-                            <li><a href="#"><i class="fa fa-bell-o"></i> Notification</a></li>
                             <li><a href="logout.php"><i class="fa fa-key"></i> Log Out</a></li>
                         </ul>
                     </li>
